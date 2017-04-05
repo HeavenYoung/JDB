6c79cb05ea8f7a90e309f0dbb7b814a139166e1b
Heaven <314664252@qq.com>
2017-04-05 20:02:29 +0800

    Setting about

---
+++ JiaoDaBang/JiaoDaBang/Controller/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  JiaoDaBang
+//
+//  Created by Heaven on 05/04/2017.
+//  Copyright © 2017 Heaven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ JiaoDaBang/JiaoDaBang/Controller/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  JiaoDaBang
+//
+//  Created by Heaven on 05/04/2017.
+//  Copyright © 2017 Heaven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
