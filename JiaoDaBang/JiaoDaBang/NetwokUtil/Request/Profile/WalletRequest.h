34faaa81df62e14f7a5de31a301c74b3189935f1
Yangtian <314664252@qq.com>
2017-04-07 22:05:32 +0800

    我的钱网络请求

---
+++ JiaoDaBang/JiaoDaBang/NetwokUtil/Request/Profile/WalletRequest.h
@@ -0,0 +1,13 @@
+//
+//  WalletRequest.h
+//  JiaoDaBang
+//
+//  Created by ott001 on 2017/4/7.
+//  Copyright © 2017年 Heaven. All rights reserved.
+//
+
+#import "BaseRequest.h"
+
+@interface WalletRequest : BaseRequest
+
+@end
---
+++ JiaoDaBang/JiaoDaBang/NetwokUtil/Response/Profile/WalletResponse.h
@@ -0,0 +1,13 @@
+//
+//  WalletResponse.h
+//  JiaoDaBang
+//
+//  Created by ott001 on 2017/4/7.
+//  Copyright © 2017年 Heaven. All rights reserved.
+//
+
+#import "BaseResponse.h"
+
+@interface WalletResponse : BaseResponse
+
+@end
