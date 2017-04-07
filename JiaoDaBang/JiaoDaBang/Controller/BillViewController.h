c8f98001a583cab2934970d3ce65d250fe1836db
Yangtian <314664252@qq.com>
2017-04-07 20:31:52 +0800

    我的钱包，体现，账单记录

---
+++ JiaoDaBang/JiaoDaBang/Controller/BillViewController.h
@@ -0,0 +1,13 @@
+//
+//  BillViewController.h
+//  JiaoDaBang
+//
+//  Created by ott001 on 2017/4/7.
+//  Copyright © 2017年 Heaven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BillViewController : UIViewController
+
+@end
